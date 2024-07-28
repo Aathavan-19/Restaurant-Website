c8382029f8ea4ef5d95eb14e8eab562a9ae2840d
Aathavan-19 <aathavangnanam@gmail.com>
2024-07-28 13:48:31 +0530

    feat : transferred files from my other folder

---
+++ restaurant/src/App.jsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// // import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//        <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+//     </>
+//   )
+// }
+
+// export default App
+import React from 'react'
+import Navbar from './assets/components/Navbar'
+const App = () => {
+  return (
+    <>
+    <div className='overflow-x-hidden'>App</div>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ restaurant/src/assets/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='bg-white shadow-lg'>
+            <div className='container'>
+            {/* logo section */}
+                <div className='font-bold'>Logo</div>
+                {/* navlinks section */}
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
